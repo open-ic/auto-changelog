0a5e0c9a88a2e3540bb3630787331749ca91e381
Julian Jelfs <julian.jelfs@gmail.com>
2023-01-16 12:17:46 +0000

    add file-pattern option

--- src/commits.js
+++ src/commits.js
@@ -1,47 +1,74 @@
-const semver = require('semver')
-const { cmd, isLink, encodeHTML, niceDate, replaceText, getGitVersion } = require('./utils')
+const semver = require("semver");
+const {
+  cmd,
+  isLink,
+  encodeHTML,
+  niceDate,
+  replaceText,
+  getGitVersion,
+} = require("./utils");
 
-const COMMIT_SEPARATOR = '__AUTO_CHANGELOG_COMMIT_SEPARATOR__'
-const MESSAGE_SEPARATOR = '__AUTO_CHANGELOG_MESSAGE_SEPARATOR__'
-const MATCH_COMMIT = /(.*)\n(.*)\n(.*)\n(.*)\n([\S\s]+)/
-const MATCH_STATS = /(\d+) files? changed(?:, (\d+) insertions?...)?(?:, (\d+) deletions?...)?/
-const BODY_FORMAT = '%B'
-const FALLBACK_BODY_FORMAT = '%s%n%n%b'
+const COMMIT_SEPARATOR = "__AUTO_CHANGELOG_COMMIT_SEPARATOR__";
+const MESSAGE_SEPARATOR = "__AUTO_CHANGELOG_MESSAGE_SEPARATOR__";
+const MATCH_COMMIT = /(.*)\n(.*)\n(.*)\n(.*)\n([\S\s]+)/;
+const MATCH_FILES = /(.*)\n(.*)\n(.*)\n(.*)\n(.*)\n([\S\s]+)/;
+const MATCH_STATS =
+  /(\d+) files? changed(?:, (\d+) insertions?...)?(?:, (\d+) deletions?...)?/;
+const BODY_FORMAT = "%B";
+const FALLBACK_BODY_FORMAT = "%s%n%n%b";
 
 // https://help.github.com/articles/closing-issues-via-commit-messages
-const DEFAULT_FIX_PATTERN = /(?:close[sd]?|fixe?[sd]?|resolve[sd]?)\s(?:#(\d+)|(https?:\/\/.+?\/(?:issues|pull|pull-requests|merge_requests)\/(\d+)))/gi
+const DEFAULT_FIX_PATTERN =
+  /(?:close[sd]?|fixe?[sd]?|resolve[sd]?)\s(?:#(\d+)|(https?:\/\/.+?\/(?:issues|pull|pull-requests|merge_requests)\/(\d+)))/gi;
 
 const MERGE_PATTERNS = [
   /^Merge pull request #(\d+) from .+\n\n(.+)/, // Regular GitHub merge
   /^(.+) \(#(\d+)\)(?:$|\n\n)/, // Github squash merge
   /^Merged in .+ \(pull request #(\d+)\)\n\n(.+)/, // BitBucket merge
-  /^Merge branch .+ into .+\n\n(.+)[\S\s]+See merge request [^!]*!(\d+)/ // GitLab merge
-]
+  /^Merge branch .+ into .+\n\n(.+)[\S\s]+See merge request [^!]*!(\d+)/, // GitLab merge
+];
 
 const fetchCommits = async (diff, options = {}) => {
-  const format = await getLogFormat()
-  const log = await cmd(`git log ${diff} --shortstat --pretty=format:${format} ${options.appendGitLog}`)
-  return parseCommits(log, options)
-}
+  const format = await getLogFormat();
+  const log = await cmd(
+    `git log ${diff} --shortstat --pretty=format:${format} ${options.appendGitLog}`
+  );
+  return parseCommits(log, options);
+};
 
 const getLogFormat = async () => {
-  const gitVersion = await getGitVersion()
-  const bodyFormat = gitVersion && semver.gte(gitVersion, '1.7.2') ? BODY_FORMAT : FALLBACK_BODY_FORMAT
-  return `${COMMIT_SEPARATOR}%H%n%ai%n%an%n%ae%n${bodyFormat}${MESSAGE_SEPARATOR}`
-}
+  const gitVersion = await getGitVersion();
+  const bodyFormat =
+    gitVersion && semver.gte(gitVersion, "1.7.2")
+      ? BODY_FORMAT
+      : FALLBACK_BODY_FORMAT;
+  return `${COMMIT_SEPARATOR}%H%n%ai%n%an%n%ae%n${bodyFormat}${MESSAGE_SEPARATOR}`;
+};
 
-const parseCommits = (string, options = {}) => {
-  return string
+const parseCommits = async (string, options = {}) => {
+  const parsed = string
     .split(COMMIT_SEPARATOR)
     .slice(1)
-    .map(commit => parseCommit(commit, options))
-    .filter(commit => filterCommit(commit, options))
-}
+    .map((commit) => parseCommit(commit, options))
+    .filter((commit) => {
+      return filterCommit(commit, options);
+    });
+
+  if (options.filePattern) {
+    const promises = parsed.map((commit) => {
+      return fetchFiles(options.filePattern, commit);
+    });
+    const results = await Promise.all(promises);
+    return parsed.filter((_, idx) => results[idx]);
+  }
+
+  return parsed;
+};
 
 const parseCommit = (commit, options = {}) => {
-  const [, hash, date, author, email, tail] = commit.match(MATCH_COMMIT)
-  const [body, stats] = tail.split(MESSAGE_SEPARATOR)
-  const message = encodeHTML(body)
+  const [, hash, date, author, email, tail] = commit.match(MATCH_COMMIT);
+  const [body, stats] = tail.split(MESSAGE_SEPARATOR);
+  const message = encodeHTML(body);
   const parsed = {
     hash,
     shorthash: hash.slice(0, 7),
@@ -53,98 +80,123 @@ const parseCommit = (commit, options = {}) => {
     message: message.trim(),
     fixes: getFixes(message, author, options),
     href: options.getCommitLink(hash),
-    breaking: !!options.breakingPattern && new RegExp(options.breakingPattern).test(message),
-    ...getStats(stats)
-  }
+    breaking:
+      !!options.breakingPattern &&
+      new RegExp(options.breakingPattern).test(message),
+    ...getStats(stats),
+  };
   return {
     ...parsed,
-    merge: getMerge(parsed, message, options)
-  }
-}
+    merge: getMerge(parsed, message, options),
+  };
+};
 
 const getSubject = (message) => {
   if (!message.trim()) {
-    return '_No commit message_'
+    return "_No commit message_";
   }
-  return message.match(/[^\n]+/)[0].trim()
-}
+  return message.match(/[^\n]+/)[0].trim();
+};
 
 const getStats = (stats) => {
-  if (!stats.trim()) return {}
-  const [, files, insertions, deletions] = stats.match(MATCH_STATS)
+  if (!stats.trim()) return {};
+  const [, files, insertions, deletions] = stats.match(MATCH_STATS);
   return {
     files: parseInt(files || 0),
     insertions: parseInt(insertions || 0),
-    deletions: parseInt(deletions || 0)
-  }
-}
+    deletions: parseInt(deletions || 0),
+  };
+};
 
 const getFixes = (message, author, options = {}) => {
-  const pattern = getFixPattern(options)
-  const fixes = []
-  let match = pattern.exec(message)
-  if (!match) return null
+  const pattern = getFixPattern(options);
+  const fixes = [];
+  let match = pattern.exec(message);
+  if (!match) return null;
   while (match) {
-    const id = getFixID(match)
-    const href = isLink(match[2]) ? match[2] : options.getIssueLink(id)
-    fixes.push({ id, href, author })
-    match = pattern.exec(message)
+    const id = getFixID(match);
+    const href = isLink(match[2]) ? match[2] : options.getIssueLink(id);
+    fixes.push({ id, href, author });
+    match = pattern.exec(message);
   }
-  return fixes
-}
+  return fixes;
+};
 
 const getFixID = (match) => {
   // Get the last non-falsey value in the match array
   for (let i = match.length; i >= 0; i--) {
     if (match[i]) {
-      return match[i]
+      return match[i];
     }
   }
-}
+};
 
 const getFixPattern = (options) => {
   if (options.issuePattern) {
-    return new RegExp(options.issuePattern, 'g')
+    return new RegExp(options.issuePattern, "g");
   }
-  return DEFAULT_FIX_PATTERN
-}
+  return DEFAULT_FIX_PATTERN;
+};
 
 const getMergePatterns = (options) => {
   if (options.mergePattern) {
-    return MERGE_PATTERNS.concat(new RegExp(options.mergePattern, 'g'))
+    return MERGE_PATTERNS.concat(new RegExp(options.mergePattern, "g"));
   }
-  return MERGE_PATTERNS
-}
+  return MERGE_PATTERNS;
+};
 
 const getMerge = (commit, message, options = {}) => {
-  const patterns = getMergePatterns(options)
+  const patterns = getMergePatterns(options);
   for (const pattern of patterns) {
-    const match = pattern.exec(message)
+    const match = pattern.exec(message);
     if (match) {
-      const id = /^\d+$/.test(match[1]) ? match[1] : match[2]
-      const message = /^\d+$/.test(match[1]) ? match[2] : match[1]
+      const id = /^\d+$/.test(match[1]) ? match[1] : match[2];
+      const message = /^\d+$/.test(match[1]) ? match[2] : match[1];
       return {
         id,
         message: replaceText(message, options),
         href: options.getMergeLink(id),
         author: commit.author,
-        commit
-      }
+        commit,
+      };
     }
   }
-  return null
-}
+  return null;
+};
 
 const filterCommit = (commit, { ignoreCommitPattern }) => {
-  if (ignoreCommitPattern && new RegExp(ignoreCommitPattern).test(commit.subject)) {
-    return false
+  if (
+    ignoreCommitPattern &&
+    new RegExp(ignoreCommitPattern).test(commit.subject)
+  ) {
+    return false;
   }
-  return true
-}
+  return true;
+};
+
+const fetchFiles = async (pattern, { hash }) => {
+  const files = await cmd(`git show --name-only ${hash}`);
+  if (files) {
+    try {
+      const [, _hash, _author, _date, , _msg, filestr] =
+        files.match(MATCH_FILES);
+      const fileList = filestr.split(/\n/);
+      const match = fileList
+        .filter((file) => file !== "")
+        .reduce((match, file) => {
+          return match || new RegExp(pattern).test(file);
+        }, false);
+      return match;
+    } catch (err) {
+      return false;
+    }
+  }
+  return false;
+};
 
 module.exports = {
   COMMIT_SEPARATOR,
   MESSAGE_SEPARATOR,
   fetchCommits,
-  parseCommit
-}
+  parseCommit,
+};
--- src/utils.js
+++ src/utils.js
@@ -1,103 +1,122 @@
-const readline = require('readline')
-const fs = require('fs')
-const { spawn } = require('child_process')
+const readline = require("readline");
+const fs = require("fs");
+const { spawn, exec } = require("child_process");
 
-const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const MONTH_NAMES = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const updateLog = (string, clearLine = true) => {
   if (clearLine) {
-    readline.clearLine(process.stdout)
-    readline.cursorTo(process.stdout, 0)
+    readline.clearLine(process.stdout);
+    readline.cursorTo(process.stdout, 0);
   }
-  process.stdout.write(`auto-changelog: ${string}`)
-}
+  process.stdout.write(`auto-changelog: ${string}`);
+};
 
 const formatBytes = (bytes) => {
-  return `${Math.max(1, Math.round(bytes / 1024))} kB`
-}
+  return `${Math.max(1, Math.round(bytes / 1024))} kB`;
+};
 
 // Simple util for calling a child process
 const cmd = (string, onProgress) => {
-  const [cmd, ...args] = string.trim().split(' ')
+  const [cmd, ...args] = string.trim().split(" ");
   return new Promise((resolve, reject) => {
-    const child = spawn(cmd, args)
-    let data = ''
+    const child = spawn(cmd, args);
+    let data = "";
 
-    child.stdout.on('data', buffer => {
-      data += buffer.toString()
+    child.stdout.on("data", (buffer) => {
+      data += buffer.toString();
       if (onProgress) {
-        onProgress(data.length)
+        onProgress(data.length);
       }
-    })
-    child.stdout.on('end', () => resolve(data))
-    child.on('error', reject)
-  })
-}
+    });
+    child.stdout.on("end", () => resolve(data));
+    child.on("error", (err) => {
+      console.error(err);
+      reject(err);
+    });
+  });
+};
 
 const getGitVersion = async () => {
-  const output = await cmd('git --version')
-  const match = output.match(/\d+\.\d+\.\d+/)
-  return match ? match[0] : null
-}
+  const output = await cmd("git --version");
+  const match = output.match(/\d+\.\d+\.\d+/);
+  return match ? match[0] : null;
+};
 
 const niceDate = (string) => {
-  const date = new Date(string)
-  const day = date.getUTCDate()
-  const month = MONTH_NAMES[date.getUTCMonth()]
-  const year = date.getUTCFullYear()
-  return `${day} ${month} ${year}`
-}
+  const date = new Date(string);
+  const day = date.getUTCDate();
+  const month = MONTH_NAMES[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+  return `${day} ${month} ${year}`;
+};
 
 const isLink = (string) => {
-  return /^http/.test(string)
-}
+  return /^http/.test(string);
+};
 
 const parseLimit = (limit) => {
-  return limit === 'false' ? false : parseInt(limit, 10)
-}
+  return limit === "false" ? false : parseInt(limit, 10);
+};
 
 const encodeHTML = (string) => {
-  return string.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-}
+  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+};
 
 const replaceText = (string, options) => {
   if (!options.replaceText) {
-    return string
+    return string;
   }
   return Object.keys(options.replaceText).reduce((string, pattern) => {
-    return string.replace(new RegExp(pattern, 'g'), options.replaceText[pattern])
-  }, string)
-}
+    return string.replace(
+      new RegExp(pattern, "g"),
+      options.replaceText[pattern]
+    );
+  }, string);
+};
 
 const createCallback = (resolve, reject) => (err, data) => {
-  if (err) reject(err)
-  else resolve(data)
-}
+  if (err) reject(err);
+  else resolve(data);
+};
 
 const readFile = (path) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', createCallback(resolve, reject))
-  })
-}
+    fs.readFile(path, "utf-8", createCallback(resolve, reject));
+  });
+};
 
 const writeFile = (path, data) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(path, data, createCallback(resolve, reject))
-  })
-}
+    fs.writeFile(path, data, createCallback(resolve, reject));
+  });
+};
 
 const fileExists = (path) => {
-  return new Promise(resolve => {
-    fs.access(path, err => resolve(!err))
-  })
-}
+  return new Promise((resolve) => {
+    fs.access(path, (err) => resolve(!err));
+  });
+};
 
 const readJson = async (path) => {
-  if (await fileExists(path) === false) {
-    return null
+  if ((await fileExists(path)) === false) {
+    return null;
   }
-  return JSON.parse(await readFile(path))
-}
+  return JSON.parse(await readFile(path));
+};
 
 module.exports = {
   updateLog,
@@ -112,5 +131,5 @@ module.exports = {
   readFile,
   writeFile,
   fileExists,
-  readJson
-}
+  readJson,
+};
